08c1421281a1b375d517f605bcc613c859e12f09
Stefan M <stefan.murakami@gmail.com>
2024-04-17 17:32:15 -0400

    add styling to search component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 export const routes: Routes = [
-    {path:'', component:HomeComponent }
+    {path:'', component:HomeComponent },
+    {path:'search/:searchTerm', component:HomeComponent }
 ];
 
 @NgModule({
